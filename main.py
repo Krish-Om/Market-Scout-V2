885148ae7f598f8ccf56e0bd08c9fd4dfcc13d48
Krish-Om <krishombasukala@gmail.com>
2026-08-04 12:43:55 +0545

    feat: Update CORS configuration to use Config for origins

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
 from app.routes import router as generation_router
 from fastapi.middleware.cors import CORSMiddleware
-from app.config import ORIGINS
+from app.config import Config
 
 app_desc = """
 # TikTok Script Generation API
 This API orchestrates a multi-step pipeline to generate TikTok scripts based on clothing descriptions and context notes. It leverages a Trend Spotter, Scriptwriter, and Guardrail QA Check to ensure high-quality script outputs.
 """
-app = FastAPI(description=app_desc, title="TikTok Script Generation API", version="1.0.0")
+app = FastAPI(
+    description=app_desc, title="TikTok Script Generation API", version="1.0.0"
+)
 
 # CORS configuration
 origins = [
     "http://localhost",
     "http://localhost:8000",
     "http://localhost:3000",
-    ORIGINS
+    Config.ORIGINS,
 ]
 
 app.add_middleware(
